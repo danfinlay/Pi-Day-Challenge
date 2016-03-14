947e5b2f1f2ac16883a4640d52768922ea8601e4
Dan Finlay <dan@danfinlay.com>
2016-03-14 14:59:01 -0500

    Cheatingish solution
    I had to use Math.PI to give the Math.sin() function its input in radians!
    
    In my opinion this is cheating, and I'd like to find a more fair way.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 module.exports = function() {
-  return Math.PI
+
+  var sideCount = 10000
+
+  var first = {x:0, y:1}
+
+  // https://en.wikipedia.org/wiki/Euler%27s_formula
+  var angle = (Math.PI * 2) / sideCount
+  var second = {
+    x: Math.sin(angle),
+    y: Math.cos(angle),
+  }
+
+  // Pythagorean theorem: a^2 + b^2 = c^2
+  var a = second.x - first.x
+  var b = first.y - second.y
+  var cSquared = Math.pow(a, 2) + Math.pow(b, 2)
+  var c = Math.sqrt(cSquared)
+
+  var circumference =  c * sideCount
+  var diameter = 2
+
+  return circumference / diameter
 }
